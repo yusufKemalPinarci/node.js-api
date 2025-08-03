6c74d3aaf18925d2f75a409ee094fb491b0bef3b
Yusuf Kemal <yusuf@rnek.com>
2025-08-03 15:00:33 +0300

    shop index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const serviceRoutes = require('./routes/service');
 const appointmentRoutes = require('./routes/appointment');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.error('MongoDB connection error:', err));
 
+  
+app.use('/api/shop', shopRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/service', serviceRoutes);
 app.use('/api/appointment', appointmentRoutes);
