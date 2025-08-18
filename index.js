1cbe14ce65e4a006a74b01347e92ee42beb174b2
Yusuf PINARCI <yusuf.pinarci@ozakglobal.com>
2025-08-18 13:55:12 +0300

    swagger

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
-
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('MongoDB connected!'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 const serviceRoutes = require('./routes/service');
 const appointmentRoutes = require('./routes/appointment');
 const shopRoutes = require('./routes/shop');
-const cors = require('cors');
+
+// 🔥 Swagger import
+const swaggerDocs = require('./swagger'); // <- bunu ekledik
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware'ler
 app.use(express.json());
-
-
-
-
 app.use(cors());
+
+// API rotaları
 app.use('/api/shop', shopRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/service', serviceRoutes);
 app.use('/api/appointment', appointmentRoutes);
 
+// 🔥 Swagger route'u aktif et
+swaggerDocs(app); // <- bunu da ekledik
 
+// MongoDB bağlantısı
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Sunucuyu başlat
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
-
---
+++ swagger.jsx
@@ -0,0 +1,35 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Node.js API Dokümantasyonu',
+      version: '1.0.0',
+    },
+     servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local Geliştirme Sunucusu',
+      },
+      {
+        url: 'https://node-js-api-8m2g.onrender.com',
+        description: 'Render Production Sunucusu',
+      },
+      {
+        url: process.env.SWAGGER_URL, // istersen dinamik olarak .env'den de al
+        description: 'Ortam Değişkeninden Gelen URL',
+      },
+    ],
+  },
+  apis: ['./routes/*.jsx'], // yorumları bu dosyalarda arayacak
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+function swaggerDocs(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = swaggerDocs;
